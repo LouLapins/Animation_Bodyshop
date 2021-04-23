cf51c15bb94ab1af0a1a311fc2e0808686c88277
LouLapins <loulapins@hotmail.com>
2021-04-23 23:21:26 +0200

    add more styling

--- js/main.js
+++ js/main.js
@@ -4,20 +4,9 @@ window.onload = function() {
 
 }
 
-
-
-
-
-
-// carousel.style.transform = 'translateX(' + (-size * counter) + 'px)';
-
-
 function slideCarousel() {
 
-    const carousel = document.querySelector('#img-carousel');
     const carouselImages = document.querySelectorAll('#img-carousel img');
-    const image = document.querySelector('.product-img');
-
 
     //Buttons
     const prevBtn = document.querySelector('#prev-btn');
@@ -30,9 +19,7 @@ function slideCarousel() {
     nextBtn.addEventListener("click", () => {
         console.log(counter);
         if (counter >= (carouselImages.length)) {
-
             counter = 0;
-
         }
 
         gsap.to(".product-img", { duration: 3, x: (-size * counter), ease: "elastic" });
@@ -47,9 +34,6 @@ function slideCarousel() {
 
         gsap.to(".product-img", { duration: 3, x: (-size * counter), ease: "elastic" });
         counter--;
-
     });
-
-
 
 }
